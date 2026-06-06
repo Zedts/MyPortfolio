5e449b9d1cf7465df5069748fc3d35fe5a329417
Zedts <savana.cell152@gmail.com>
2026-06-06 12:38:52 +0700

    feat: Add achievement

--- lib/data.ts
+++ lib/data.ts
@@ -13,6 +13,7 @@ export const SOCIAL_LINKS = [
     { name: 'github', url: 'https://github.com/Zedts' },
     { name: 'linkedin', url: 'https://www.linkedin.com/in/royyan-hikmal-kautsar-a406332b0/' },
     { name: 'instagram', url: 'https://www.instagram.com/royyan.hk/' },
+    { name: 'achievement', url: 'https://drive.google.com/drive/folders/1s61XgX6NXa1-G-8EMMepBoPOOQaTJ_iN?usp=sharing' },
 ];
 
 export const MY_STACK = {
